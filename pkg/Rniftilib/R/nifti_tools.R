0cb16ef8c6120385ad6644361098b0e1110d2232
olg <olg@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2009-06-12 19:15:29 +0000

    reorganisation of R files
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/rniftilib@9 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/Rniftilib/R/nifti_tools.R
+++ pkg/Rniftilib/R/nifti_tools.R
@@ -1,13 +1,24 @@
+displayDTI <- function(v1, v2, v3, ...)
+{
+  colvec <- rgb(abs(v1), abs(v2), abs(v3))
+  colors <- unique(colvec)
+  colmat <- array(match(colvec, colors), dim=dim(v1)[1:2])
+  image(x = 1:(dim(colmat)[2]), y=1:(dim(colmat)[1]),
+        z = t(colmat), col=colors,
+        xlab="", ylab="", axes=FALSE, asp=1, ...)
+}
+
+
 CreateVOIMask <- function(templatefile=file.choose(),
                           output.basefilename, MNI, radius=2)
 {
   nifti.img <- nifti.image.read(templatefile)
   nifti.set.filenames(nifti.img, output.basefilename)
   # show the result
-  #print(nifti.img$"fname")
-  #print(nifti.img$"iname")
-  nifti.img$"scl_inter" <- 0
-  nifti.img$"scl_slope" <- 1
+  #print(nifti.img$fname)
+  #print(nifti.img$iname)
+  nifti.img$"scl.inter" <- 0
+  nifti.img$"scl.slope" <- 1
 
   #mval <- max(nifti.img[])
   for (x in 1:dim(nifti.img)[1])
@@ -16,7 +27,7 @@ CreateVOIMask <- function(templatefile=file.choose(),
       {
         for (z in 1:dim(nifti.img)[3])
           {
-            pos.mm <- nifti.img$"qto_xyz" %*% c(x-1,y-1,z-1,1)
+            pos.mm <- nifti.img$qto.xyz %*% c(x-1,y-1,z-1,1)
             if (sqrt(sum((MNI-pos.mm[1:3])^2)) <= radius)
               { 
                 nifti.img[x,y,z] <- 1
