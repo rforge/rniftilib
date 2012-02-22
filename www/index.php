2cd55afe6e5f815834aa035111c90dede48d11e8
olg <olg@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2012-02-22 08:08:58 +0000

    add user manual and reference to publication (documentation)
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/rniftilib@42 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- www/index.php
+++ www/index.php
@@ -51,12 +51,17 @@ Additional information about the reference implementation  and the <a href="http
 <!-- end of project description -->
 
 <p>Further information as well as source and binary packages can be found on the <a href="http://r-forge.r-project.org/projects/rniftilib/">project summary page</a>.</p>
+<hr />
 <h2 class="headline">Rniftitools: software package based on Rniftilib</h2>
 <p>This is a software package to trace the head surface from MRI image data and generate a triangle mesh surface.
 This software was originally used in a <a href="http://en.wikipedia.org/wiki/Transcranial_magnetic_stimulation"> transcranial magnetic stimulation (TMS)</a> 
 experiment to find a good stimulation site. This software is experimental research software and should be use with caution!
 </p>
-<p>Package source code: <a href="Rniftitools_0.0-1.tar.gz">Rniftitools_0.0-1.tar.gz</a></p>
+<h3>References</h3>
+<p><b>Phonological decisions require both the left and right supramarginal gyri</b>; Hartwigsen G, Baumgaertner A, Price CJ, Koehnke M, Ulmer S, Siebner HR.; 
+Proc Natl Acad Sci U S A. 2010 Sep 21;107(38):16494-9. Epub 2010 Aug 31. PMID: <a href="http://www.ncbi.nlm.nih.gov/pubmed?term=PMID%3A%2020807747">20807747</a><p>
+<h3>Downloads</h3>
+<p>Package source code  : <a href="Rniftitools_0.0-1.tar.gz">Rniftitools_0.0-1.tar.gz</a>  User manual : <a href="Rniftitools_0.0-1.pdf">Rniftitools_0.0-1.pdf</a></p>
 <img src="nifti_iconBlue_small.png" border="0" alt="* " />
 </body>
 </html>
